9d623ee5345cec8217882c6b8d1b5dd072375b0f
Vedarth Ambilkar <vedarthambilkar2@gmail.com>
2024-09-26 09:59:18 +0530

    web app initialization

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
+import withPWAInit from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+    dest: "public",
+	cacheOnFrontEndNav: true,
+	aggressiveFrontEndNavCaching: true,
+	reloadOnOnline: true,
+	swcMinify: true,
+	disable: process.env.NODE_ENV === "development",
+	workboxOptions: {
+		disableDevLogs: true,
+	},
+});
+
+export default withPWA(nextConfig);

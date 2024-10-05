85c845c1a9e13cc8d0d29c6e8b3cf326a8924f98
vinayrewatkar <vinayrewatkar.257@gmail.com>
2024-10-05 10:16:16 +0530

    create routing folders

---
+++ src/app/police/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const police = () => {
+  return (
+    <div>police</div>
+  )
+}
+
+export default police
---
+++ src/app/policyMaker/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const policyMaker = () => {
+  return (
+    <div>policyMakers</div>
+  )
+}
+
+export default policyMaker

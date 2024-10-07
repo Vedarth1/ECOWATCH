d43890e7db559b026645e67d29c8f429fa894831
vinayrewatkar <vinayrewatkar.257@gmail.com>
2024-10-07 21:22:45 +0530

    init

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import CountCard from "../dashboard/components/counterCard"
+'use client'
+import React, { useEffect, useState } from 'react';
+import { useWebSocket } from '../../hooks/webSocket';
+import { ImageProcessingResponse } from '../../types/ImageProcessing';
 
-const dashboard = () => {
+const Dashboard = () => {
+  const { message, isConnected } = useWebSocket('ws://localhost:8000/socket.io/?EIO=4&transport=websocket'); // Update with your server URL
+  const [receivedData, setReceivedData] = useState<ImageProcessingResponse | null>(null); // Update type here
+
+  useEffect(() => {
+    if (message) {
+      console.log('Message received from server:', message);
+      setReceivedData(message); // Set the received message directly
+    }
+  }, [message]);
+
   return (
-    <><div>dashboard</div><CountCard /></>
-  )
-}
+    <div>
+      <div>Dashboard {isConnected ? '(Connected)' : '(Disconnected)'}</div>
+      <div>
+        <h3>Data from WebSocket:</h3>
+        {receivedData ? (
+          <pre>{JSON.stringify(receivedData)}</pre>
+        ) : (
+          <div>No data received yet...</div>
+        )}
+      </div>
+    </div>
+  );
+};
 
-export default dashboard
+export default Dashboard;

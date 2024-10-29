6712472d512796b8c8110ae48045033d04c51011
vinayrewatkar <vinayrewatkar.257@gmail.com>
2024-10-29 15:48:55 +0530

    create reports count card

--- src/app/policyMaker/page.tsx
+++ src/app/policyMaker/page.tsx
@@ -1,10 +1,42 @@
-import React from 'react'
-import Reports from './components/reports'
+import React from 'react';
+import Reports from './components/reports';
+import Counts from './components/counts';
 
-const policyMaker = () => {
+const PolicyMaker = () => {
+  const metrics = [
+    {
+      count: 12,
+      name: "Two wheelers"
+    },
+    {
+      count: 56,
+      name: "Cars(LMV)"
+    },
+    {
+      count: 89,
+      name: "Public buses"
+    }
+  ];
+
   return (
-    <Reports/>
-  )
-}
+    <div className="px-6">
+      <div className="space-y-6">
+        <div className="text-white text-xl text-center font-semibold">
+          Invalid PUC
+        </div>
+        <div className="grid grid-cols-3 gap-4">
+          {metrics.map((metric, index) => (
+            <Counts
+              key={index}
+              count={metric.count}
+              name={metric.name}
+            />
+          ))}
+        </div>
+        <Reports />
+      </div>
+    </div>
+  );
+};
 
-export default policyMaker
+export default PolicyMaker;
